27e3d831da6017de7bfdfb7bad3d2eacd15e35e0
César R. Contreras <c1sar90@gmail.com>
2022-05-01 20:24:23 -0500

    feat: add default value to Lato font family

--- tailwind/horizon/font-family.js
+++ tailwind/horizon/font-family.js
@@ -1,4 +1,5 @@
 module.exports = {
+  sans: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
   lato: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
   montserrat: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif'],
   inconsolata: ['Inconsolata', 'monospace', 'Arial', 'sans-serif'],
