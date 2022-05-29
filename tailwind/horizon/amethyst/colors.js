d5e7956517aaeb557b43da17f859c75b61d05544
César R. Contreras <c1sar90@gmail.com>
2022-05-29 14:28:20 -0500

    feat: add horizon color themes

---
+++ tailwind/horizon/amethyst/colors.js
@@ -0,0 +1,72 @@
+module.exports = {
+	transparent: 'transparent',
+	primary: {
+		main: {
+			900: '#1D0134',
+			800: '#2D0051',
+			700: '#4B0086',
+			600: '#6800BC',
+			500: '#8600F2',
+			400: '#A028FF',
+			300: '#B75EFF',
+			200: '#CF94FF',
+			100: '#E7C9FF',
+		},
+		neutral: {
+      900: '#222231',
+      800: '#3A3A4D',
+      700: '#4B4A60',
+      600: '#54546B',
+			500: '#666582',
+			400: '#8483A5',
+			300: '#A2A1C8',
+			200: '#C2C1E0',
+			100: '#EAEAFF',
+		},
+		wb: {
+			white: '#FFFFFF',
+			black: '#000000',
+		},
+	},
+	secondary: {
+		main: {
+      900: '#590050',
+      800: '#8E007F',
+      700: '#BC00A8',
+      600: '#ED00D4',
+			500: '#FF37EA',
+			400: '#FF5CEE',
+			300: '#FF88F2',
+			200: '#FFADF6',
+			100: '#FFDAFB',
+		},
+	},
+	feedback: {
+		danger: {
+			300: '#FF4329',
+			200: '#FF8473',
+			100: '#FFC2BA',
+		},
+		attention: {
+			300: '#FFBD3E',
+			200: '#FFDA93',
+			100: '#FFE8BB',
+		},
+		success: {
+			300: '#9AFE6B',
+			200: '#C2FF9D',
+			100: '#E8FFC4',
+		},
+	},
+  gradient: {
+    amethyst: {
+      100: '#F9D1FF',
+    },
+    charoite: {
+      100: '#020B56'
+    },
+    rhodolite: {
+      100: '#E8008B'
+    },
+  },
+}
