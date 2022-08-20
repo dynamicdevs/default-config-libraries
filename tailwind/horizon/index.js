b690f9292dfdcf7e671df936834844c050235605
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-08-19 21:00:01 -0500

    feat: add custom background image and animation values

--- tailwind/horizon/index.js
+++ tailwind/horizon/index.js
@@ -12,7 +12,23 @@ module.exports= {
       '4xl': '2rem',
     },
     spacing: spacings,
-    maxWidth: spacings
+    maxWidth: spacings,
+    backgroundImage: (theme) => ({
+      'roots-pattern': 'linear-gradient(112.78deg, #4F76FF 14.79%, #9D00E7 85.21%)'
+    }),
+    keyframes: {
+      rotation: {
+        '0%': {
+          transform: 'rotate(0deg)'
+        },
+        '100%': {
+          transform: 'rotate(360deg)'
+        }
+      }
+    },
+    animation: {
+      'loader-rotation': 'rotation 1s linear infinite'
+    }
   },
   container: () => ({
     center: true,
