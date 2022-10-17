a80b74c6cdf75ee4a5659362a467eec0d5d6cb58
zituz <cracker.slow134@gmail.com>
2022-10-17 14:47:04 +0200

    chore: add gradient color to yumit setting

--- tailwind/yumit/index.js
+++ tailwind/yumit/index.js
@@ -1,20 +1,23 @@
-const colors = require('./colors');
-const fontFamily = require('./font-family');
-const fontSize = require('../horizon/font-size');
-const screens = require('../default/screens');
-const spacings = require('../default/spacings');
-const shadows = require('../default/shadows');
+const colors = require("./colors");
+const fontFamily = require("./font-family");
+const fontSize = require("../horizon/font-size");
+const screens = require("../default/screens");
+const spacings = require("../default/spacings");
+const shadows = require("../default/shadows");
 
-module.exports= {
+module.exports = {
   screens: screens,
   extend: {
     borderRadius: {
-      '4xl': '2rem',
+      "4xl": "2rem",
     },
     spacing: spacings,
     maxWidth: spacings,
     backgroundImage: (theme) => ({
-      'roots-pattern': 'linear-gradient(112.78deg, #4F76FF 14.79%, #9D00E7 85.21%)'
+      "roots-pattern":
+        "linear-gradient(112.78deg, #4F76FF 14.79%, #9D00E7 85.21%)",
+      "hover-pattern":
+        "linear-gradient(39.89deg, #FDD245 -1.57%, #FE2260 61.38%, rgba(6, 6, 30, 0) 100%);",
     }),
   },
   container: () => ({
@@ -25,6 +28,6 @@ module.exports= {
   colors: colors,
   boxShadow: shadows,
   fill: (theme) => ({
-    ...theme('colors'),
+    ...theme("colors"),
   }),
-}
+};
