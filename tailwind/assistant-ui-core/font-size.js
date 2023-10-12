82f787c3fe112ce8e0eb24b1614a09cbfd2f7e3c
Lucho Morales Segobia <luchoms09@gmail.com>
2023-10-12 14:16:13 -0500

    chore: add more fonts size

--- tailwind/assistant-ui-core/font-size.js
+++ tailwind/assistant-ui-core/font-size.js
@@ -1,4 +1,40 @@
 module.exports = {
+  'heading-01': [
+    '64px',
+    {
+      lineHeight: '88px',
+    },
+  ],
+  'heading-02': [
+    '56px',
+    {
+      lineHeight: '80px',
+    },
+  ],
+  'heading-03': [
+    '48px',
+    {
+      lineHeight: '64px',
+    },
+  ],
+  'heading-04': [
+    '40px',
+    {
+      lineHeight: '56px',
+    },
+  ],
+  'heading-05': [
+    '32px',
+    {
+      lineHeight: '48px',
+    },
+  ],
+  'heading-06': [
+    '24px',
+    {
+      lineHeight: '32px',
+    },
+  ],
   'display-01': [
     '36px',
     {
